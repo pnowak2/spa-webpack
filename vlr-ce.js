d76406ab1b21295f1487bc5dc994a54b2ceab86c
Piotr Nowak <p.nowak2@gmail.com>
2016-08-28 21:11:33 +0200

    body styles

--- vlr-ce.js
+++ vlr-ce.js
@@ -1,7 +1,7 @@
 var $ = require('jquery'),
-  Modernizr = require('modernizr'),
-  SearchBoxComponent = require('./app/shared/components/searching/search-box/main.component'),
-  searchBoxComponent = new SearchBoxComponent();
+  styles = require('./styles/style.css');
+Modernizr = require('modernizr'),
+SearchBoxComponent = require('./app/shared/components/searching/search-box/main.component'),
+searchBoxComponent = new SearchBoxComponent();
 
 $('body').html(searchBoxComponent.render().view.el);
-console.log(Modernizr);
